639fd1cff55bb7fc6dc8fd2a1fae24f78af33bd9
thetamimahmed <wwwtamim000@gmail.com>
2023-05-20 03:35:58 +0600

    added post method

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
         res.send(result)
     })
 
+    //post toy car
+    app.post("/toys", async(req, res)=>{
+        const toy = req.body;
+        const result = await toyCollection.insertOne(toy)
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
