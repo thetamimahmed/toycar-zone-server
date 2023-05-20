87b37b2d6c494546b2988f32a72eadfec9b987b4
thetamimahmed <wwwtamim000@gmail.com>
2023-05-20 20:23:51 +0600

    deploy in vercel

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
     const toyCollection = client.db("toyCarZoneDB").collection("toyCar");
     //get all toyCar
     app.get("/toys", async(req, res)=>{
