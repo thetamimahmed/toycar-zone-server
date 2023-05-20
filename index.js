807a532cab5b1d81c3ede5fcd69102c283c355d9
thetamimahmed <wwwtamim000@gmail.com>
2023-05-20 14:48:34 +0600

    add limit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ async function run() {
     const toyCollection = client.db("toyCarZoneDB").collection("toyCar");
     //get all toyCar
     app.get("/toys", async(req, res)=>{
-        const result = await toyCollection.find({}).toArray();
+        const result = await toyCollection.find({}).limit(20).toArray();
         res.send(result)
     })
 
